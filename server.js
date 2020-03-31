7d04ee238602cd554ac43f274aab874ff5448f7e
Michael Fernandez <michael_f2005@hotmail.com>
2020-03-31 18:13:07 -0500

    PRIMER COMMIT

---
+++ server.js
@@ -0,0 +1,19 @@
+// Comentario 1
+console.log("Hola mundo!");
+console.log("Lorem ipsum dolor sit amet");
+var n = "Michael";
+var ln = "Fernandez";
+console.log("Soy " + n + " " + ln + "!");
+/*
+Comentario 2
+*/
+
+var precio = 2000;
+var cantidad = 3;
+var descuento = 0.2;
+var totalVenta = precio * cantidad;
+console.log("Total antes de decuento: " + totalVenta);
+console.log("Valor del decuento: " + (totalVenta*descuento));
+console.log("Total a pagar: " + (totalVenta-(totalVenta*descuento)) );
+
+console.log("El nuevo estado es 'pendiente'");
