3fa9ea937e052d72547b7051954273672e736ed3
Michael Fernandez <michael_f2005@hotmail.com>
2020-03-31 20:07:46 -0500

    Calculo salario con extras

---
+++ condicionalesSalario.js
@@ -0,0 +1,22 @@
+// entradas
+var horasTrabajadas = 45;
+var valorHora = 2000;
+
+// calcular salario base
+var salario = horasTrabajadas * valorHora;
+
+// calcular extras
+var horasExtra = horasTrabajadas - 40;
+if(horasExtra < 0){
+    horasExtra = 0;
+}
+var valorExtra  = horasExtra * (valorHora*0.5);
+
+// calcular salario total
+var totalSalario = salario + valorExtra;
+
+// salida
+console.log("Valor Hora: " + valorHora);
+console.log("Salario Base (" + horasTrabajadas + " horas): " + salario);
+console.log("Valor Extra (" + horasExtra + " horas) :" + valorExtra);
+console.log("Salario TOTAL: " + totalSalario);
