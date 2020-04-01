9c8d19b9cb0d5bbc527502584bee016c898e6de3
Michael Fernandez <michael_f2005@hotmail.com>
2020-03-31 19:17:54 -0500

    Condicionales anidados

---
+++ condicionales3.js
@@ -0,0 +1,11 @@
+var num = 11;
+
+if (num < 10) {
+  console.log("El número es menor a 10");
+} else {
+  if (num > 10) {
+    console.log("El número es mayor a 10");  
+  } else {
+    console.log("El número es igual a 10");
+  }
+}
