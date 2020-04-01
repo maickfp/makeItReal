c78e9c762b06f22daf3ebbf4416b1bbc80ead1f4
Michael Fernandez <michael_f2005@hotmail.com>
2020-03-31 20:17:52 -0500

    Condicionales - mayor de tres numeros positivos diferentes

---
+++ condicionalesNumMayor.js
@@ -0,0 +1,19 @@
+// etradas
+var num1 = 5;
+var num2 = 10;
+var num3 = 3;
+
+// calcular mayor
+var mayor = 0;
+if(num1 > mayor){
+    mayor = num1;
+}
+if(num2 > mayor){
+    mayor = num2;
+}
+if(num3 > mayor){
+    mayor = num3;
+}
+
+// salida
+console.log("El número mayor es: " + mayor);
