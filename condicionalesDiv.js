0c829c54497b504deda5b20baaefe16151cc39d2
Michael Fernandez <michael_f2005@hotmail.com>
2020-03-31 19:44:10 -0500

    Ejercicion de división

---
+++ condicionalesDiv.js
@@ -0,0 +1,11 @@
+// entradas
+var divisor = 9;
+var dividendo = 3;
+
+// verificacion del dividor
+if(divisor == 0){
+    console.log("El divisor debe ser diferente a 0");
+// calcular division
+}else{
+    console.log("El resultado de la división (" + divisor + "/" + dividendo + ") es: " + (divisor/dividendo));
+}
